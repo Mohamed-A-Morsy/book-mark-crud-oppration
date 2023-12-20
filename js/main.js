5f48621467f8be288ff85099de69f298303f9d86
Mohamed-A-Morsy <dev.mohamed.moorsy@gmail.com>
2023-12-21 00:41:58 +0200

    add localStarage feature

--- js/main.js
+++ js/main.js
@@ -4,18 +4,27 @@ var submit = document.getElementById("submit");
 
 var allSites = [];
 
+if (localStorage.getItem("allSites") != null) {
+
+    allSites = JSON.parse(localStorage.getItem("allSites"));
+    displayAllSites();
+}
+
 function addSites() {
     var site = {
         index: 0,
         name: siteNameInput.value,
         siteUrl: siteUrlInput.value,
     }
-
-    allSites.push(site);
+if(submit.innerHTML =="ADD"){
+     allSites.push(site);
     localStorage.setItem("allSites", JSON.stringify(allSites));
+}else{
+    update();
+}
+   
     clearInputs();
     displayAllSites();
-
 
 }
 function clearInputs() {
@@ -30,7 +39,6 @@ function displayAllSites() {
         <tr>
         <td>${i + 1}</td>
         <td>${allSites[i].name}</td>
-
         <td><a href="${allSites[i].siteUrl}"target="_blank"><button class="btn btn-success"><span><i class="fa-solid fa-eye pe-1" style="color: #ffffff;"></i></span> Visit</button></a></td>
         <td><button class="btn btn-danger" onclick="deleteSite(${i});"> <span><i  class="fa-solid fa-trash-can pe-1" style="color: #ffffff;"></i></span>Delete</button> </td>
         <td><button class="btn btn-secondary" onclick=" update(${i})"> <span><i class="fa-solid fa-pen pe-1" style="color: #ffffff;"></i></span>Update</button></td>
@@ -38,13 +46,13 @@ function displayAllSites() {
         `
     }
     document.getElementById("tbody").innerHTML = cartona;
-    
 
 }
 
 function deleteSite(index) {
 
     allSites.splice(index, 1);
+    localStorage.setItem("allSites", JSON.stringify(allSites));
     displayAllSites();
 }
 function update(index) {
@@ -56,14 +64,13 @@ function update(index) {
             name: siteNameInput.value,
             siteUrl: siteUrlInput.value
         }
-
-
         allSites[index].name = allSites[index].name
         allSites[index].siteUrl = allSites[index].siteUrl
         allSites.splice(index, 1, updatedSite)
+        localStorage.setItem("allSites", JSON.stringify(allSites));
         clearInputs();
         submit.innerHTML = "ADD"
-        submit.onclick = addSites();
+        submit.onclick = addSites;
         displayAllSites();
     }
 
